6c4697b7e903f8ffe6cc4df42cbf6ab2238e09f6
sun <xxsunxxrose@gmail.com>
2021-06-18 13:19:36 +0530

    fixed color

--- discord/notification.py
+++ discord/notification.py
@@ -8,7 +8,7 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 webhook = DiscordWebhook(url=url)
 
 
-embed = DiscordEmbed(title='Gravity Ethereum Node ', description=f'Successfully build and pushed to [dockerhub](https://hub.docker.com/r/leeway302/gravity_eth_image1) at time {str(datetime.now())} ', color='03b2f8')
+embed = DiscordEmbed(title='Gravity Ethereum Node ', description=f'```yaml\nSuccessfully build and pushed to [dockerhub](https://hub.docker.com/r/leeway302/gravity_eth_image1) at time {str(datetime.now())}```', color='03b2f8')
 embed.set_thumbnail(url="https://raw.githubusercontent.com/onomyprotocol/gravity-bridge/main/gravity-bridge.svg")
 embed.set_author(name='Khanjan', url='https://github.com/khanjanlee/gravity-bridge', icon_url='https://media.glassdoor.com/sqll/343398/leewayhertz-squarelogo-1425384496934.png')
 webhook.add_embed(embed)
