d75e55c8fb4ce26b250dc44f830439020ba755ce
sun <xxsunxxrose@gmail.com>
2021-06-18 13:39:30 +0530

    fixed logo

--- discord/notification.py
+++ discord/notification.py
@@ -8,7 +8,7 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 webhook = DiscordWebhook(url=url)
 
 
-embed = DiscordEmbed(title='Gravity Ethereum Node ', description=f'```yaml\nSuccessfully build and pushed to Docker Hub at time {str(datetime.now())}```', color='03b2f8')
+embed = DiscordEmbed(title=' <:ether:855358106342129664> Gravity Ethereum Node ', description=f'```yaml\nSuccessfully build and pushed to Docker Hub at time {str(datetime.now())}```', color='03b2f8')
 embed.set_thumbnail(url="https://raw.githubusercontent.com/onomyprotocol/gravity-bridge/main/gravity-bridge.svg")
 embed.set_author(name='Khanjan', url='https://github.com/khanjanlee/gravity-bridge', icon_url='https://media.glassdoor.com/sqll/343398/leewayhertz-squarelogo-1425384496934.png')
 embed.add_embed_field(name="**DockerHub**" , value="<:docker:855356086772039680> ** [gravity_eth_image1](https://hub.docker.com/r/leeway302/gravity_eth_image1) **")
