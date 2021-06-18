7bb97ff43c13d2424cb202f69538c898cc578fb6
sun <xxsunxxrose@gmail.com>
2021-06-18 13:04:40 +0530

    prepared everything

--- discord/notification.py
+++ discord/notification.py
@@ -1,6 +1,6 @@
 
 
-url = "https://discord.com/api/webhooks/855345877279703051/PMbAVWOohJrJreAUOtgNyFWGjYUCWoXUY5Uryw2tv5y2UXmEM4CxVyTHVJwsUCKHZ4iM"
+url = "https://discord.com/api/webhooks/855349363277692969/8upsNV_Hr74JuLmdfH6MOBzY8y5BUISXoa7jdkUiNYQ3Z9XmhBqIRkQP3sGp4Pmi7-UL"
 from datetime import datetime
 
 from discord_webhook import DiscordWebhook, DiscordEmbed
@@ -11,7 +11,7 @@ webhook = DiscordWebhook(url=url)
 # you can set the color as a decimal (color=242424) or hex (color='03b2f8') number
 embed = DiscordEmbed(title='Gravity Ethereum Node ', description=f'Successfully build and pushed to [dockerhub](https://hub.docker.com/r/leeway302/gravity_eth_image1) at time {str(datetime.now())} ', color='03b2f8')
 embed.set_thumbnail(url="https://raw.githubusercontent.com/onomyprotocol/gravity-bridge/main/gravity-bridge.svg")
-embed.set_author(name='Leewayhertz', url='https://github.com/khanjanlee/gravity-bridge', icon_url='https://media.glassdoor.com/sqll/343398/leewayhertz-squarelogo-1425384496934.png')
+embed.set_author(name='Khanjan', url='https://github.com/khanjanlee/gravity-bridge', icon_url='https://media.glassdoor.com/sqll/343398/leewayhertz-squarelogo-1425384496934.png')
 # add embed object to webhook
 webhook.add_embed(embed)
 
