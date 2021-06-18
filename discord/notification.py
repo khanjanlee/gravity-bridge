651866157626e0b77f4f0f47f01704dbf74de3f3
sun <xxsunxxrose@gmail.com>
2021-06-18 14:00:26 +0530

    fixed aligns

--- discord/notification.py
+++ discord/notification.py
@@ -11,8 +11,8 @@ OS = os.environ["RUNNER_OS"]
 NAME = os.environ["GITHUB_WORKFLOW"]
 COMMIT = os.environ["GITHUB_SHA"]
 ID = os.environ["GITHUB_ACTION"]
-
-embed = DiscordEmbed(title=' <:ether:855358106342129664> Gravity Ethereum Node ', description=f'```yaml\nAction : {ID} \nStatus: Successfully build\nCommit : {NAME}\n Commot Sha : {COMMIT}\nOperating System: {OS}\nTime : {str(datetime.now())}```', color='03b2f8')
+EVENT = os.environ["GITHUB_EVENT_NAME"]
+embed = DiscordEmbed(title=' <:ether:855358106342129664> Gravity Ethereum Node ', description=f'```yaml\nAction : {ID} \nStatus: Successfully build\nEvent : {EVENT}\nCommit : {NAME}\nCommit Hash : {COMMIT}\nOperating System: {OS}\nTime : {str(datetime.now())}```', color='03b2f8')
 embed.set_thumbnail(url="https://raw.githubusercontent.com/onomyprotocol/gravity-bridge/main/gravity-bridge.svg")
 embed.set_author(name='Khanjan', url='https://github.com/khanjanlee/gravity-bridge', icon_url='https://media.glassdoor.com/sqll/343398/leewayhertz-squarelogo-1425384496934.png')
 embed.add_embed_field(name="**DockerHub**" , value="<:docker:855356086772039680> ** [gravity_eth_image1](https://hub.docker.com/r/leeway302/gravity_eth_image1) **")
