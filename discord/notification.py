2ab2ea1fb465798554596d27a6bb5379e98887f9
sun <xxsunxxrose@gmail.com>
2021-06-18 13:55:10 +0530

    update

--- discord/notification.py
+++ discord/notification.py
@@ -12,7 +12,7 @@ NAME = os.environ["GITHUB_WORKFLOW"]
 COMMIT = os.environ["GITHUB_SHA"]
 ID = os.environ["GITHUB_ACTION"]
 
-embed = DiscordEmbed(title=' <:ether:855358106342129664> Gravity Ethereum Node ', description=f'```yaml\nAction {ID} : Successfully build commit {NAME}  , sha : {COMMIT} on {OS} and pushed to Docker Hub at time {str(datetime.now())}\n', color='03b2f8')
+embed = DiscordEmbed(title=' <:ether:855358106342129664> Gravity Ethereum Node ', description=f'```yaml\nAction {ID} : Successfully build commit {NAME}  , sha : {COMMIT} on {OS} and pushed to Docker Hub at time {str(datetime.now())}```', color='03b2f8')
 embed.set_thumbnail(url="https://raw.githubusercontent.com/onomyprotocol/gravity-bridge/main/gravity-bridge.svg")
 embed.set_author(name='Khanjan', url='https://github.com/khanjanlee/gravity-bridge', icon_url='https://media.glassdoor.com/sqll/343398/leewayhertz-squarelogo-1425384496934.png')
 embed.add_embed_field(name="**DockerHub**" , value="<:docker:855356086772039680> ** [gravity_eth_image1](https://hub.docker.com/r/leeway302/gravity_eth_image1) **")
