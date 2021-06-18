8d85e66ba4b06666a799d4badedd6a40fe93521d
sun <xxsunxxrose@gmail.com>
2021-06-18 13:17:18 +0530

    removed unwanted stuff

--- discord/notification.py
+++ discord/notification.py
@@ -7,12 +7,10 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 
 webhook = DiscordWebhook(url=url)
 
-# create embed object for webhook
-# you can set the color as a decimal (color=242424) or hex (color='03b2f8') number
+
 embed = DiscordEmbed(title='Gravity Ethereum Node ', description=f'Successfully build and pushed to [dockerhub](https://hub.docker.com/r/leeway302/gravity_eth_image1) at time {str(datetime.now())} ', color='03b2f8')
 embed.set_thumbnail(url="https://raw.githubusercontent.com/onomyprotocol/gravity-bridge/main/gravity-bridge.svg")
 embed.set_author(name='Khanjan', url='https://github.com/khanjanlee/gravity-bridge', icon_url='https://media.glassdoor.com/sqll/343398/leewayhertz-squarelogo-1425384496934.png')
-# add embed object to webhook
 webhook.add_embed(embed)
 
 response = webhook.execute()
